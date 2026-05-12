8d140f13effba5d1563a7efeec049cbbb0fce0ba
Kirill <kirillvolostnov16@gmail.com>
2026-05-12 20:53:35 +0300

    fix1

--- auth_service/app/db.py
+++ auth_service/app/db.py
@@ -3,8 +3,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = os.getenv("AUTH_DATABASE_URL", "postgresql+psycopg2://postgres:postgres@localhost:5432/auth_db")
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("AUTH_DATABASE_URL", "sqlite:///./auth.db")
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
